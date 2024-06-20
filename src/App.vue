0283eb02f08c20cd19671e2f414fc98930bf2722
Jordan Farrow <jordan.farrow@gmail.com>
2024-06-20 10:14:02 -0700

    functional graph and UI for requested data; includes currency and p/l formatting

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
-    <CodingChallenge :optionsData="sampleData"/>
+    <CodingChallenge :optionsData="sampleData" />
+    <p class="copyright">&copy; 2024 <a href="mailto:jordan.farrow@gmail.com">Jordan Farrow</a></p>
   </div>
 </template>
 
 <script>
 import CodingChallenge from './components/CodingChallenge.vue'
-
 export default {
   name: 'App',
   components: {
@@ -16,43 +16,42 @@ export default {
     return {
       sampleData: [
         {
-          "strike_price": 100, 
-          "type": "Call", 
-          "bid": 10.05, 
-          "ask": 12.04, 
-          "long_short": "long", 
+          "strike_price": 100,
+          "type": "Call",
+          "bid": 10.05,
+          "ask": 12.04,
+          "long_short": "long",
           "expiration_date": "2025-12-17T00:00:00Z"
         },
         {
-          "strike_price": 102.50, 
-          "type": "Call", 
-          "bid": 12.10, 
-          "ask": 14, 
-          "long_short": "long", 
+          "strike_price": 102.50,
+          "type": "Call",
+          "bid": 12.10,
+          "ask": 14,
+          "long_short": "long",
           "expiration_date": "2025-12-17T00:00:00Z"
         },
         {
-          "strike_price": 103, 
-          "type": "Put", 
-          "bid": 14, 
-          "ask": 15.50, 
-          "long_short": "short", 
+          "strike_price": 103,
+          "type": "Put",
+          "bid": 14,
+          "ask": 15.50,
+          "long_short": "short",
           "expiration_date": "2025-12-17T00:00:00Z"
         },
         {
-          "strike_price": 105, 
-          "type": "Put", 
-          "bid": 16, 
-          "ask": 18, 
-          "long_short": "long", 
+          "strike_price": 105,
+          "type": "Put",
+          "bid": 16,
+          "ask": 18,
+          "long_short": "long",
           "expiration_date": "2025-12-17T00:00:00Z"
         }
-      ]
+      ],
+
     }
   }
 }
-
-
 </script>
 
 <style>
@@ -62,10 +61,18 @@ export default {
   font-style: normal;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin:60px 10vw;
+  margin: 60px 10vw;
 }
+
+LineChart {
+  width: 80vw;
+  height: 80vh;
+  border: 1px solid #787888;
+  display: block;
+}
+
 .fira-sans-regular {
   font-family: "Fira Sans", sans-serif;
   font-weight: 400;
@@ -83,6 +90,26 @@ export default {
   font-weight: 900;
   font-style: normal;
 }
-h1,h2,h3,h4,h5{ font-weight: 900;}
-strong,b{ font-weight: 700; }
+
+h1,
+h2,
+h3,
+h4,
+h5 {
+  font-weight: 900;
+}
+
+strong,
+b {
+  font-weight: 700;
+}
+
+:link {
+  color: #c09;
+}
+.copyright{
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+}
 </style>
--- src/components/CodingChallenge.vue
+++ src/components/CodingChallenge.vue
@@ -1,47 +1,138 @@
 <template>
   <div>
     <h1>Options Profit Calculator</h1>
-    Data <pre>{{ optionsData }}</pre>
-    <Line :data="data" :options="options" />
+    <pre>    {{ chartOptions }}</pre>
+    <LineChart :data="data" :options="chartOptions" />
+    <OptionsCard :cardData="optionsData" ref="cards" />
+
   </div>
 </template>
 
 <script>
+const optionContractQty = 100;
+const unlimitedX = 1.5; // used to plot graph for unlimited gain/loss 
+const palette = ['rgba(0, 63, 92, 1)', 'rgba(0, 129, 155, 1)', 'rgba(0, 196, 148, 1)', 'rgba(126, 255, 66, 1)'];
+
 import {
-    Chart as ChartJS,
-    Title,
-    Tooltip,
-    Legend,
-    BarElement,
-    CategoryScale,
-    LinearScale
-  } from 'chart.js'
-  import { Line } from 'vue-chartjs'
-  ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js';
+import { Line as LineChart } from 'vue-chartjs';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+import OptionsCard from './OptionsCard.vue';
 
 export default {
   name: 'CodingChallenge',
   props: {
-    optionsData: Array
+    optionsData: [],
+    chartOptions:{
+      type: Object,
+      default: ()=>{}
+    }
   },
-  components:{
-    Line
+  components: {
+    LineChart,
+    OptionsCard
   },
   data() {
-      return {
-        data: {
-          labels: ['January', 'February', 'March'],
-          datasets: [{ data: [40, 20, 12] }]
-        },
-        options: {
-          responsive: true
+    //process optionsData for max p/l, be for each
+    //options contract represents 100 of underlying asset
+
+    let xAxisLabels = [];
+    let lineDatasets = [];
+    for (let od = 0; od < this.optionsData.length; od++) {
+      let contract = this.optionsData[od];
+      xAxisLabels.push(contract.strike_price + "");
+      // buy or sell?
+      let type = contract.type;
+      if (contract.long_short === "long") {
+        if (type === "Call") {
+          //long call
+          contract.maxProfit = contract.strike_price * unlimitedX;
+          contract.maxLoss = contract.bid * optionContractQty;
+          contract.breakEven = contract.strike_price + contract.bid;
+          contract.lineData = [{ x: contract.strike_price - 5, y: -contract.bid }, { x: contract.strike_price, y: -contract.bid }, { x: contract.strike_price, y: 0 }, { x: contract.strike_price + 5, y: contract.maxProfit }];
+        } else {
+          //long Put
+          contract.maxProfit = contract.strike_price - contract.bid;
+          contract.maxLoss = contract.ask;
+          contract.breakEven = contract.strike_price - contract.ask;
+          contract.lineData = [{ x: contract.strike_price - 5, y: contract.ask }, { x: contract.strike_price, y: contract.ask }, { x: contract.strike_price, y: 0 }, { x: contract.strike_price + 5, y: contract.maxLoss }];
+
+        }
+      } else {
+        if (type === "Put") {
+          //short put
+          contract.maxProfit = contract.bid;
+          contract.maxLoss = contract.strike_price * unlimitedX;
+          contract.breakEven = contract.strike_price - contract.bid;
+          contract.lineData = [{ x: contract.strike_price - 5, y: contract.ask }, { x: contract.strike_price, y: contract.ask }, { x: contract.strike_price, y: 0 }, { x: contract.strike_price + 5, y: contract.maxLoss }];
+
+        } else {
+          //short call option
+          contract.maxProfit = contract.bid;
+          contract.maxLoss = (contract.strike_price + contract.bid) - contract.ask;
+          contract.breakEven = contract.strike_price + contract.bid;
+          contract.lineData = [{ x: contract.strike_price - 5, y: -contract.bid }, { x: contract.strike_price, y: -contract.bid }, { x: contract.strike_price, y: 0 }, { x: contract.strike_price + 5, y: contract.maxProfit }];
+
         }
       }
+      contract.label = `${type} option (${contract.long_short})`;
+      contract.color = palette[od];
+
+      lineDatasets.push({
+        label: contract.label,
+        fill: false,
+        borderColor: contract.color,
+        data: contract.lineData
+      });
     }
-  // Your code here
+ 
+    return {
+      data: {
+        type: 'line',
+        labels: xAxisLabels,
+        datasets: lineDatasets,
+        chartOptions: {
+          scales: {
+            yAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: 'Profit/Loss'
+              }
+            }],
+            y: {
+              maxTicksLimit: 10 // Limit the number of ticks on the y-axis
+            },
+            x: {
+              maxTicksLimit: 10 // Limit the number of ticks on the x-axis
+            }
+          },
+          interaction: {
+            intersect: false,
+          }
+        },
+        optionsData: this.optionsData,
+        unlimitedX: this.unlimitedX
+      }
+    }
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
